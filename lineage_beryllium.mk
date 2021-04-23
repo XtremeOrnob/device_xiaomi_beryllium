7bf4c65dae0ab53d779f4a54ca26be347e5ab0fb
Xtreme.Ornob <xtreme.ornob@gmail.com>
2021-04-23 09:03:16 +0000

    beryllium: Bringup ProjectSakura!

--- lineage_beryllium.mk
+++ lineage_beryllium.mk
@@ -9,6 +9,25 @@ $(call inherit-product, device/xiaomi/beryllium/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Sakura build type
+SAKURA_OFFICIAL := false
+
+# Gapps Sector
+SAKURA_BUILD_TYPE := gapps
+SAKURA_GAPPS := true
+TARGET_GAPPS_ARCH := arm64
+IS_PHONE := true
+
+# Bootlogo sector
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SCREEN_HEIGHT := 2246
+TARGET_SCREEN_WIDTH := 1080
+# Maintainer Sector
+SAKURA_MAINTAINER := XtremeOrnob
+
+# FaceUnlock Sector
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_beryllium
 PRODUCT_DEVICE := beryllium
@@ -16,10 +35,13 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := POCO F1
 PRODUCT_MANUFACTURER := Xiaomi
 
-BUILD_FINGERPRINT := "Xiaomi/beryllium/beryllium:10/QKQ1.190828.002/V12.0.3.0.QEJMIXM:user/release-keys"
+BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ2A.210405.005/7181113:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="beryllium-user 10 QKQ1.190828.002 V12.0.3.0.QEJMIXM release-keys" \
+    PRIVATE_BUILD_DESC="redfin-user 11 RQ2A.210405.005 7181113 release-keys" \
     PRODUCT_NAME="beryllium"
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.fingerprint=google/redfin/redfin:11/RQ2A.210405.005/7181113:user/release-keys
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi-rev1
