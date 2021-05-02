6b8122cd6cc7741c91ca43cdbca4875f6def4727
XtremeOrnobadsaacacd <xtreme.ornob@gmail.com>
2021-05-02 12:31:16 +0000

    Revert "beryllium: remove conflicting overlay"
    This reverts commit 1b0cf2a4c77d964e8df2e1fef54b7a99468061b2.

--- device.mk
+++ device.mk
@@ -24,6 +24,8 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
+PRODUCT_PACKAGES += \
+    NoCutoutOverlay
 
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
